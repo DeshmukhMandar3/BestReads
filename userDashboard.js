6e153c6f4acf8da15c15867cfe783e84ad5a7e31
Rizwank123 <expertkhan7@gmail.com>
2022-11-10 19:22:14 +0530

    err

--- userDashboard.js
+++ userDashboard.js
@@ -1,22 +1,36 @@
-// tab switching 
-let btn_click=document.querySelectorAll('.btn_container');
-let nav =document.querySelectorAll('.nav-tab');
-const tabSwitch=(index,color)=>{
-    btn_click.forEach((node)=>{
-        node.style.backgroundColor="";
-        node.style.color="";
+import navbar_fn from "./components/navbar.js"
+    let nav_div=document.getElementById("navbar");
+    nav_div.innerHTML=navbar_fn();
 
-    });
-    btn_click[index].style.backgroundColor='white';
-    btn_click[index].style.color=color;
-    nav.forEach((node)=>{
-        node.style.display='none';
-    });
-    console.log(index);
-    nav[index].style.display="block";
-   
-}
-tabSwitch(0,' #7b7b7b');
-// tab switch end 
+    // navbar code snippet start
+    let ddn_btn=document.querySelector(".ddn-name");
+    ddn_btn.onclick=()=>{
+        let dropdown_class=document.getElementById("dropdown-content");
+        show_hide(dropdown_class);
+    }
+
+    let signIn_btn=document.getElementById("SignIn");
+    signIn_btn.onclick=()=>{
+        let SignIn_class=document.getElementById("signIn-ddn");
+        show_hide(SignIn_class);
+    }
+    const show_hide=(list)=>{
+    
+       
+         list.classList.toggle("show");
+    }
+    //when user logins, set item with local_name as key and name of user as value in local storage
+    let local_name=localStorage.getItem("local_name");
+    if(local_name!=null){
+        let log_btn=document.getElementById("log-name");
+        log_btn.innerText=local_name;
+        let ddn_name=document.querySelector(".ddn-name");
+        ddn_name.style.display="block";
+        let sign=document.getElementById("sign")
+        sign.style.display="none";
+        let sign_up=document.getElementById("sign-up")
+        sign_up.style.display="none";
+    }
 
 
+    // navbar code snippet end
